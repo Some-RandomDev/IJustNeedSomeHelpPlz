b076f9dd1e59d9f3bee473270561523aecbdbbc3
Some-randomDev <prakharchandra5506@gmai.com>
2020-11-05 12:50:09 +0530

    New Update: The code is now more broke

--- Game_loop.py
+++ Game_loop.py
@@ -1,6 +1,6 @@
 import Setup
 Setup. Create_screen()
-while Setup.health != 0:
+while True:
     Setup. draw_compass()
     Setup. draw_health()
     Setup. draw_health()
@@ -9,3 +9,4 @@ while Setup.health != 0:
     Setup. press_loop()
     Setup. refresh_heading()
     Setup. t. clear()
+    Setup. t. done()
